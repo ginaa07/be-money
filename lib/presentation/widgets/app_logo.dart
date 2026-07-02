ca3dfaeb6d6a6e33c3ff6a6a15bd2f8656fd4551
ginaa07 <1123150124@global.ac.id>
2026-07-03 01:29:30 +0700

    feat: update name text in app_logo.dart

--- lib/presentation/widgets/app_logo.dart
+++ lib/presentation/widgets/app_logo.dart
@@ -31,7 +31,7 @@ class AppLogo extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Dompet Kampus',
+              'Dompet Kita',
               style: TextStyle(
                 fontFamily: fontFamily,
                 fontSize: size * 0.3,
